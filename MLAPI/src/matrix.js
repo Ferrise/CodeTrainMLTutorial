d50f4d603fea734312e128ec7a91effbb038c9b9
Ferris Esposito <ferrisesposito@gmail.com>
2018-07-12 15:49:01 -0400

    Account for matrix product in multiply

--- MLAPI/src/matrix.js
+++ MLAPI/src/matrix.js
@@ -38,12 +38,24 @@ var Matrix = class {
     }
 
     multiply(n) {
+        // Uses Matrix product AB
         if(n instanceof Matrix) {
-            for(let i = 0; i < this.rows; i++) {
-                for(let j = 0; j < this.cols; j++) {
-                    this.matrix[i][j] *= n.matrix[i][j];
+            if(this.cols !== n.rows) { return undefined; }
+            let result = new Matrix(this.rows, n.cols);
+            let a = this.matrix;
+            let b = n.matrix;
+
+            for(let i = 0; i < result.rows; i++) {
+                for(let j = 0; j < result.cols; j++) {
+                    // Dot product of values in col
+                    let sum = 0;
+                    for(let k = 0; k < this.cols; k++) {
+                        sum += a[i][k] * b[k][j];
+                    }
+                    result.matrix[i][j] = sum;
                 }
             }
+            return result;
         } else {
             for(let i = 0; i < this.rows; i++) {
                 for(let j = 0; j < this.cols; j++) {
--- MLAPI/src/sketch.js
+++ MLAPI/src/sketch.js
@@ -2,7 +2,15 @@ var brain;
 
 function setput() {
     brain = new NeuralNetwork(3, 3, 1);
-    
+    let a = new Matrix(2, 3);
+    let b = new Matrix(3, 2);
+    a.randomize();
+    b.randomize();
+    console.table(a.matrix);
+    console.table(b.matrix);
+
+    let c = a.multiply(b);
+    console.table(c.matrix);
 }
 
 function draw() {
