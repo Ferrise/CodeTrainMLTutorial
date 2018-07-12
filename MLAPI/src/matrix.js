ee51e26e73a3147639a480615f98a432d201ecf8
Ferris Esposito <ferrisesposito@gmail.com>
2018-07-12 12:29:58 -0400

    Add sum and multiply (dot product) to matrix class

--- MLAPI/src/matrix.js
+++ MLAPI/src/matrix.js
@@ -2,6 +2,29 @@ var Matrix = class {
     constructor(rows, cols) {
         this.rows = rows;
         this.cols = cols;
-        
+        this.matrix = [];
+
+        for(let i = 0; i < this.rows; i++) {
+            this.matrix[i] = [];
+            for(let j = 0; j < this.cols; j++) {
+                this.matrix[i][j] = 0;
+            }
+        }
+    }
+
+    sum(n) {
+        for(let i = 0; i < this.rows; i++) {
+            for(let j = 0; j < this.cols; j++) {
+                this.matrix[i][j] += n;
+            }
+        }
+    }
+
+    multiply(n) {
+        for(let i = 0; i < this.rows; i++) {
+            for(let j = 0; j < this.cols; j++) {
+                this.matrix[i][j] *= n;
+            }
+        }
     }
 }
